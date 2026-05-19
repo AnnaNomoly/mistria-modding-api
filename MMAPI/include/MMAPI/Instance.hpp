7a058e6ba6a977c0de910c386a32d695352dbd6b
AnnaNomoly <annanomoly@outlook.com>
2026-05-18 20:49:00 -0400

    fix: Updated Instancemodule to use correct obj_monster names

--- MMAPI/include/MMAPI/Instance.hpp
+++ MMAPI/include/MMAPI/Instance.hpp
@@ -33,8 +33,27 @@ namespace MMAPI::Instance
 		WorldFountain,
 
 		// Monsters
-		Monster,
+		MonsterBarrel,
+		MonsterBat,
+		MonsterBatSonicAttack,
+		MonsterBatSonicBoom,
+		MonsterCat,
+		MonsterClod,
+		MonsterClodBomb,
+		MonsterClodProjectile,
+		MonsterEnchantern,
+		MonsterEnchanternProjectile,
 		MonsterMimic,
+		MonsterMite,
+		MonsterMiteSecondary,
+		MonsterRockStack,
+		MonsterSap,
+		MonsterShroom,
+		MonsterSpirit,
+		MonsterSpiritProjectile,
+		MonsterStars,
+		MonsterStatue,
+		MonsterTome,
 
 		// Dungeon
 		Dragonshrine,
@@ -148,54 +167,73 @@ namespace MMAPI::Instance
 		{
 			switch (obj)
 			{
-				case MMAPI::Instance::Objects::Ari:                return "obj_ari";
-				case MMAPI::Instance::Objects::AssetObject:        return "obj_assetobject";
-				case MMAPI::Instance::Objects::Bug:                return "obj_bug";
-				case MMAPI::Instance::Objects::Door:               return "obj_door";
-				case MMAPI::Instance::Objects::FarmBell:           return "obj_farm_bell";
-				case MMAPI::Instance::Objects::Fish:               return "obj_fishy";
-				case MMAPI::Instance::Objects::WorldFountain:      return "obj_world_fountain";
-				case MMAPI::Instance::Objects::Monster:            return "obj_monster";
-				case MMAPI::Instance::Objects::MonsterMimic:       return "obj_monster_mimic";
-				case MMAPI::Instance::Objects::Dragonshrine:       return "obj_dragonshrine";
-				case MMAPI::Instance::Objects::DungeonElevator:    return "obj_dungeon_elevator";
-				case MMAPI::Instance::Objects::DungeonLadderDown:  return "obj_dungeon_ladder_down";
-				case MMAPI::Instance::Objects::DungeonRitualAltar: return "obj_dungeon_ritual_altar";
-				case MMAPI::Instance::Objects::Adeline:            return "obj_adeline";
-				case MMAPI::Instance::Objects::Balor:              return "obj_balor";
-				case MMAPI::Instance::Objects::Caldarus:           return "obj_caldarus";
-				case MMAPI::Instance::Objects::Celine:             return "obj_celine";
-				case MMAPI::Instance::Objects::Darcy:              return "obj_darcy";
-				case MMAPI::Instance::Objects::Dell:               return "obj_dell";
-				case MMAPI::Instance::Objects::Dozy:               return "obj_dozy";
-				case MMAPI::Instance::Objects::Eiland:             return "obj_eiland";
-				case MMAPI::Instance::Objects::Elsie:              return "obj_elsie";
-				case MMAPI::Instance::Objects::Errol:              return "obj_errol";
-				case MMAPI::Instance::Objects::Hayden:             return "obj_hayden";
-				case MMAPI::Instance::Objects::Hemlock:            return "obj_hemlock";
-				case MMAPI::Instance::Objects::Henrietta:          return "obj_henrietta";
-				case MMAPI::Instance::Objects::Holt:               return "obj_holt";
-				case MMAPI::Instance::Objects::Josephine:          return "obj_josephine";
-				case MMAPI::Instance::Objects::Juniper:            return "obj_juniper";
-				case MMAPI::Instance::Objects::Landen:             return "obj_landen";
-				case MMAPI::Instance::Objects::Louis:              return "obj_louis";
-				case MMAPI::Instance::Objects::Luc:                return "obj_luc";
-				case MMAPI::Instance::Objects::Maple:              return "obj_maple";
-				case MMAPI::Instance::Objects::March:              return "obj_march";
-				case MMAPI::Instance::Objects::Merri:              return "obj_merri";
-				case MMAPI::Instance::Objects::Nora:               return "obj_nora";
-				case MMAPI::Instance::Objects::Olric:              return "obj_olric";
-				case MMAPI::Instance::Objects::Reina:              return "obj_reina";
-				case MMAPI::Instance::Objects::Ryis:               return "obj_ryis";
-				case MMAPI::Instance::Objects::Seridia:            return "obj_seridia";
-				case MMAPI::Instance::Objects::Stillwell:          return "obj_stillwell";
-				case MMAPI::Instance::Objects::Taliferro:          return "obj_taliferro";
-				case MMAPI::Instance::Objects::Terithia:           return "obj_terithia";
-				case MMAPI::Instance::Objects::Valen:              return "obj_valen";
-				case MMAPI::Instance::Objects::Vera:               return "obj_vera";
-				case MMAPI::Instance::Objects::Wheedle:            return "obj_wheedle";
-				case MMAPI::Instance::Objects::Zorel:              return "obj_zorel";
-				default:                                           return nullptr;
+				case MMAPI::Instance::Objects::Ari:                         return "obj_ari";
+				case MMAPI::Instance::Objects::AssetObject:                 return "obj_assetobject";
+				case MMAPI::Instance::Objects::Bug:                         return "obj_bug";
+				case MMAPI::Instance::Objects::Door:                        return "obj_door";
+				case MMAPI::Instance::Objects::FarmBell:                    return "obj_farm_bell";
+				case MMAPI::Instance::Objects::Fish:                        return "obj_fishy";
+				case MMAPI::Instance::Objects::WorldFountain:               return "obj_world_fountain";
+				case MMAPI::Instance::Objects::MonsterBarrel:               return "obj_monster_barrel";
+				case MMAPI::Instance::Objects::MonsterBat:                  return "obj_monster_bat";
+				case MMAPI::Instance::Objects::MonsterBatSonicAttack:       return "obj_monster_bat_sonic_attack";
+				case MMAPI::Instance::Objects::MonsterBatSonicBoom:         return "obj_monster_bat_sonic_boom";
+				case MMAPI::Instance::Objects::MonsterCat:                  return "obj_monster_cat";
+				case MMAPI::Instance::Objects::MonsterClod:                 return "obj_monster_clod";
+				case MMAPI::Instance::Objects::MonsterClodBomb:             return "obj_monster_clod_bomb";
+				case MMAPI::Instance::Objects::MonsterClodProjectile:       return "obj_monster_clod_projectile";
+				case MMAPI::Instance::Objects::MonsterEnchantern:           return "obj_monster_enchantern";
+				case MMAPI::Instance::Objects::MonsterEnchanternProjectile: return "obj_monster_enchantern_projectile";
+				case MMAPI::Instance::Objects::MonsterMimic:                return "obj_monster_mimic";
+				case MMAPI::Instance::Objects::MonsterMite:                 return "obj_monster_mite";
+				case MMAPI::Instance::Objects::MonsterMiteSecondary:        return "obj_monster_mite_secondary";
+				case MMAPI::Instance::Objects::MonsterRockStack:            return "obj_monster_rock_stack";
+				case MMAPI::Instance::Objects::MonsterSap:                  return "obj_monster_sap";
+				case MMAPI::Instance::Objects::MonsterShroom:               return "obj_monster_shroom";
+				case MMAPI::Instance::Objects::MonsterSpirit:               return "obj_monster_spirit";
+				case MMAPI::Instance::Objects::MonsterSpiritProjectile:     return "obj_monster_spirit_projectile";
+				case MMAPI::Instance::Objects::MonsterStars:                return "obj_monster_stars";
+				case MMAPI::Instance::Objects::MonsterStatue:               return "obj_monster_statue";
+				case MMAPI::Instance::Objects::MonsterTome:                 return "obj_monster_tome";
+				case MMAPI::Instance::Objects::Dragonshrine:                return "obj_dragonshrine";
+				case MMAPI::Instance::Objects::DungeonElevator:             return "obj_dungeon_elevator";
+				case MMAPI::Instance::Objects::DungeonLadderDown:           return "obj_dungeon_ladder_down";
+				case MMAPI::Instance::Objects::DungeonRitualAltar:          return "obj_dungeon_ritual_altar";
+				case MMAPI::Instance::Objects::Adeline:                     return "obj_adeline";
+				case MMAPI::Instance::Objects::Balor:                       return "obj_balor";
+				case MMAPI::Instance::Objects::Caldarus:                    return "obj_caldarus";
+				case MMAPI::Instance::Objects::Celine:                      return "obj_celine";
+				case MMAPI::Instance::Objects::Darcy:                       return "obj_darcy";
+				case MMAPI::Instance::Objects::Dell:                        return "obj_dell";
+				case MMAPI::Instance::Objects::Dozy:                        return "obj_dozy";
+				case MMAPI::Instance::Objects::Eiland:                      return "obj_eiland";
+				case MMAPI::Instance::Objects::Elsie:                       return "obj_elsie";
+				case MMAPI::Instance::Objects::Errol:                       return "obj_errol";
+				case MMAPI::Instance::Objects::Hayden:                      return "obj_hayden";
+				case MMAPI::Instance::Objects::Hemlock:                     return "obj_hemlock";
+				case MMAPI::Instance::Objects::Henrietta:                   return "obj_henrietta";
+				case MMAPI::Instance::Objects::Holt:                        return "obj_holt";
+				case MMAPI::Instance::Objects::Josephine:                   return "obj_josephine";
+				case MMAPI::Instance::Objects::Juniper:                     return "obj_juniper";
+				case MMAPI::Instance::Objects::Landen:                      return "obj_landen";
+				case MMAPI::Instance::Objects::Louis:                       return "obj_louis";
+				case MMAPI::Instance::Objects::Luc:                         return "obj_luc";
+				case MMAPI::Instance::Objects::Maple:                       return "obj_maple";
+				case MMAPI::Instance::Objects::March:                       return "obj_march";
+				case MMAPI::Instance::Objects::Merri:                       return "obj_merri";
+				case MMAPI::Instance::Objects::Nora:                        return "obj_nora";
+				case MMAPI::Instance::Objects::Olric:                       return "obj_olric";
+				case MMAPI::Instance::Objects::Reina:                       return "obj_reina";
+				case MMAPI::Instance::Objects::Ryis:                        return "obj_ryis";
+				case MMAPI::Instance::Objects::Seridia:                     return "obj_seridia";
+				case MMAPI::Instance::Objects::Stillwell:                   return "obj_stillwell";
+				case MMAPI::Instance::Objects::Taliferro:                   return "obj_taliferro";
+				case MMAPI::Instance::Objects::Terithia:                    return "obj_terithia";
+				case MMAPI::Instance::Objects::Valen:                       return "obj_valen";
+				case MMAPI::Instance::Objects::Vera:                        return "obj_vera";
+				case MMAPI::Instance::Objects::Wheedle:                     return "obj_wheedle";
+				case MMAPI::Instance::Objects::Zorel:                       return "obj_zorel";
+				default:                                                    return nullptr;
 			}
 		}
 
